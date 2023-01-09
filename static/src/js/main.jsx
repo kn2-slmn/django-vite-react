0a41af3f4871736c23e8df62c34512fc3b55b4c5
kn2.slmn <kn2.solomon@gmail.com>
2023-01-09 12:17:45 +0800

    Identifier (project) & image render fix)

--- static/src/js/main.jsx
+++ static/src/js/main.jsx
@@ -3,8 +3,10 @@ import 'vite/modulepreload-polyfill';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import '../css/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
